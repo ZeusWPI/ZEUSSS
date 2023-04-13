759e7b30d6ce40ab7bda621c1bf900ccf0b0b617
smessie <smessie@smessie.com>
2023-04-13 23:53:19 +0200

    feat: define all api routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,10 +18,78 @@ server.get("/", (_, res) => {
 
 server.register(
   (instance, opts, next) => {
+    // GET requests
     instance.get("/teams", async (request, reply) => {
       const teams = await prisma.team.findMany();
       reply.send(teams);
     });
+    instance.get("/teams/{category}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/teams/{id}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/poules", async (request, reply) => {
+      // TODO: list of poules, joined with teams and poule_match_teams
+    });
+    instance.get("/poules/{id}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/poules/{pouleId}/matches", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/poules/{pouleId}/matches/{matchId}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/bracket", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/bracket/matches", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.get("/bracket/matches/{matchId}", async (request, reply) => {
+      // TODO: implement
+    });
+
+    // POST requests
+    instance.post("/teams", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.post("/poules", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.post("/bracket", async (request, reply) => {
+      // TODO: implement
+    });
+
+    // PATCH requests
+    instance.patch("/teams/{id}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.patch("/poules/{id}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.patch("/poules/{pouleId}/matches/{matchId}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.patch("/poules/{pouleId}/matches/{matchId}/{teamId}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.patch("/bracket/matches/{matchId}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.patch("/bracket/matches/{matchId}/{teamId}", async (request, reply) => {
+      // TODO: implement
+    });
+
+    // DELETE requests
+    instance.delete("/teams/{teamId}", async (request, reply) => {
+      // TODO: implement
+    });
+    instance.delete("/poules/{pouleId}", async (request, reply) => {
+      // TODO: implement
+    });
+
     next();
   },
   { prefix: "/api" }

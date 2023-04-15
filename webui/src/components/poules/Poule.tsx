f1f8024c976986520049e0d649126360cb14241f
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-04-15 13:06:11 +0200

    feat: poule add match info editing

--- webui/src/components/poules/Poule.tsx
+++ webui/src/components/poules/Poule.tsx
@@ -1,8 +1,10 @@
 import { fetchPouleMatches } from "@/lib/api";
-import { Card, Center, Flex, Loader, Paper, SimpleGrid, Stack, Text, Title } from "@mantine/core";
+import { Card, Center, Flex, Group, Loader, NumberInput, Paper, SimpleGrid, Stack, Text, Title } from "@mantine/core";
+import { DateTimePicker } from "@mantine/dates";
+import { notifications } from "@mantine/notifications";
 import { useQuery } from "@tanstack/react-query";
 import { AlertTriangle } from "lucide-react";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 declare type PouleProps = {
   poule: API.Poule;
@@ -16,7 +18,45 @@ export const Poule = ({poule, readonly}: PouleProps) => {
     staleTime: 30000,
   });
 
-  const sortedTeams = useMemo(() => poule.teams.sort((t1, t2) => t2.score - t1.score), [poule.teams]);
+  const sortedTeams = useMemo(() => poule.teams.sort((t1, t2) => (t2?.score ?? 0) - (t1?.score ?? 0)), [poule.teams]);
+
+  const updateTeamScore = async (matchId: number, teamId: number, score: number) => {
+    if (Number.isNaN(score)) {
+      return;
+    }
+    const resp = await fetch(`/api/poules/${poule.id}/matches/${matchId}/teams/${teamId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        score
+      })
+    });
+    const data = await resp.json();
+    if (!resp.ok) {
+      notifications.show({
+        message: `Failed to update team score, ${teamId} - ${matchId}: ${data?.message ?? resp.statusText}`,
+        color: "red",
+      });
+      return;
+    };
+  };
+
+  const updateMatchDate = async (matchId: number, playDate: Date) => {
+    if (playDate.toString() === "Invalid Date") return;
+    const resp = await fetch(`/api/poules/${poule.id}/matches/${matchId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        date: playDate.toString(),
+      })
+    });
+    const data = await resp.json();
+    if (!resp.ok) {
+      notifications.show({
+        message: `Failed to update match date, ${matchId}: ${data?.message ?? resp.statusText}`,
+        color: "red",
+      });
+      return;
+    };
+  };
 
   return (
     <Card shadow="sm" padding="lg" m='xs' radius="md" withBorder w={"19rem"} style={{overflow: "visible"}}>
@@ -56,6 +96,36 @@ export const Poule = ({poule, readonly}: PouleProps) => {
             </Stack>
           </Center>
         )}
+        {matches && matches.map(match => (
+          <Paper shadow={"sm"} p="sm" key={`match-${match.id}`}>
+            {match.teams.map(mTeam => (
+              <Group key={`match-${match.id}-team-${mTeam.id}`} position={"apart"}>
+                <Text>
+                  {mTeam.name}
+                </Text>
+                {readonly ? (
+                  <Text weight={"semibold"}>
+                    {mTeam.score}
+                  </Text>
+                ): (
+                  <NumberInput
+                    min={0}
+                    placeholder="score"
+                    value={mTeam.score ?? 0}
+                    onBlur={(v) => updateTeamScore(match.id, mTeam.id, Number(v.currentTarget.value))}
+                  />
+                )}
+              </Group>
+            ))}
+            {!readonly && (
+              <DateTimePicker
+                label="Match play moment"
+                value={new Date(match.date ?? Date.now())}
+                onDateChange={date => updateMatchDate(match.id, date)}
+              />
+            )}
+          </Paper>
+        ))}
       </Card.Section>
     </Card>
   );

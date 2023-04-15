d987e3d4e4cc1af367147bcdf44097aaa7ab621c
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-04-15 15:48:20 +0200

    feat: add footer

---
+++ webui/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { Flex, Text } from "@mantine/core";
+import { Heart } from "lucide-react";
+import vekLogo from "@/assets/vek.svg";
+
+export const Footer = () => {
+  return (
+    <Flex justify={"space-evenly"} bg="vek" p="md" className="footer">
+      <img src="https://zinc.zeus.gent/white" />
+      <Flex align={"center"} justify="center">
+        <Text color="white" align="center">
+          Made with <Heart fill="rgb(242, 78, 111)" stroke="rgb(242, 78, 111)" size={"1.1rem"}/> by Zeus WPI
+        </Text>
+      </Flex>
+      <img src={vekLogo} />
+    </Flex>
+  );
+};
--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -5,17 +5,30 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import React, { useMemo } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Bracket } from "./components/Bracket";
+import { Footer } from "./components/Footer";
+import { devBracketMatches, devBracketRounds } from "./lib/devData";
 import { queryClient } from "./lib/query";
 import { TeamContextProvider } from "./lib/stores/teamContext";
 import { theme } from "./lib/theme";
 import { router } from "./routes";
 
-const App = () => (
-  <div>
-    VEK X Zeus
-  </div>
-);
 import "./styles/index.scss";
+
+const App = () => {
+  const bracket = useMemo(() => {
+    const bracket: Brackets.Bracket = [];
+    for(let i = 0; i < devBracketRounds.rounds; i++) {
+      bracket[i] = devBracketMatches.filter(match => match.parentId === i);
+    }
+    return bracket;
+  }, []);
+  return (
+    <div>
+      <Bracket bracket={bracket} />
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -23,8 +36,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <QueryClientProvider client={queryClient}>
         <TeamContextProvider>
           <ModalsProvider>
-            <Notifications />
-            <RouterProvider router={router} />
+            <div className="content-wrapper">
+              <Notifications />
+              <RouterProvider router={router} />
+            </div>
+            <Footer />
           </ModalsProvider>
         </TeamContextProvider>
       </QueryClientProvider>

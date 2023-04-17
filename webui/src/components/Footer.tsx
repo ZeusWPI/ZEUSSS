6ccfc866803eaad76e16216aa5faf617171d67e1
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-04-17 16:20:52 +0200

    feat(Footer): add hrefs to photos

--- webui/src/components/Footer.tsx
+++ webui/src/components/Footer.tsx
@@ -5,13 +5,17 @@ import vekLogo from "@/assets/vek.svg";
 export const Footer = () => {
   return (
     <Flex justify={"space-evenly"} bg="vek" p="md" className="footer">
-      <img src="https://zinc.zeus.gent/white" />
+      <a href="https://zeus.gent">
+        <img src="https://zinc.zeus.gent/white" />
+      </a>
       <Flex align={"center"} justify="center">
         <Text color="white" align="center">
           Made with <Heart fill="rgb(242, 78, 111)" stroke="rgb(242, 78, 111)" size={"1.1rem"} /> by Zeus WPI
         </Text>
       </Flex>
-      <img src={vekLogo} />
+      <a href="https://vek.be">
+        <img src={vekLogo} />
+      </a>
     </Flex>
   );
 };

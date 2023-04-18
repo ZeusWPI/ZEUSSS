d48558204c44936db160428f3f4a2b29316292df
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-04-18 18:05:33 +0200

    chore: enable sourceMap generation

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,6 +4,9 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
